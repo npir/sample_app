5c75ae5b8b6e8c73363841db0c24a297ac69e2b5
Noorullah Pir <pir.noor@gmail.com>
2012-02-08 21:05:26 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
-	root to: 'static_pages#home'
+  get "users/new"
+  
+  match '/signup',  to: 'users#new'
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
+  root to: 'static_pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
