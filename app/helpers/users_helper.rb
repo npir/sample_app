7acd4cf60cd7f22ab369083cc36e05605c6edc4f
Noorullah Pir <pir.noor@gmail.com>
2012-02-23 23:06:51 +0000

    Finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", size: 40)
   end
 end
